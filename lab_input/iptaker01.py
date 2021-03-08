61413529248dca1f06c5921e9e66346b98fcac15
dgregory49 <dgregory49@gmail.com>
2021-03-08 17:43:59 +0000

    Labs 1-7 Complete

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter an IPv4 IP address: ")
+print("The IPv4 address is: " + user_input)
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter an IPv4 IP address: ")
+
+##This is a comment
+# print("The IPv4 address is: " + user_input)
+
+## print() use commas
+print("You told me the IPv4 address is:", user_input)
+
+vendorName = input("Enter the Vendor Name: ")
+print("Vendor:", vendorName)
