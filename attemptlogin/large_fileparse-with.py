24d2b5b35e3f876bec5ed08d623723b1c16ec2ee
dgregory49 <dgregory49@gmail.com>
2021-03-10 18:21:16 +0000

    Lab 26 complete

--- attemptlogin/large_fileparse-with.py
+++ attemptlogin/large_fileparse-with.py
@@ -2,7 +2,6 @@
 
 # parse keystone.common.wsgi and return number of failed login attempts
 loginfail = 0 # counter for fails
-loginfailindex = 1
 loginfailip = ""
 loginsuccess = 0
 # open the file for reading
@@ -13,8 +12,7 @@ with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
         # if this 'fail pattern' appears in the line...
         if "- - - - -] Authorization failed" in line:
             loginfail += 1 # this is the same as loginfail = loginfail + 1
-            loginfailip += f"\t{loginfailindex}: {line.split(' ')[-1]}"
-            loginfailindex += 1
+            loginfailip += f"\t{loginfail}: {line.split(' ')[-1]}"
         if "- - - - -] Loaded" in line:
             loginsuccess += 1
 
