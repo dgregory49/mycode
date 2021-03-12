df5e036db310289b24ba8ed096a72fcdced334bc
dgregory49 <dgregory49@gmail.com>
2021-03-12 18:21:19 +0000

    Labs 39-42 complete

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -7,10 +7,14 @@ print(netifaces.interfaces())
 def getMacAddress(address): return(netifaces.ifaddresses(address)[netifaces.AF_LINK][0]['addr'])
 def getIpAddress(address): return(netifaces.ifaddresses(address)[netifaces.AF_INET][0]['addr'])
 
-for i in netifaces.interfaces():
-    print('\n**************Details of Interface - ' + i + ' *********************')
-    try:
-        print("MAC:", getMacAddress(i))
-        print("IP:", getIpAddress(i))
-    except:
-        print('Could not collect adapter information')
+def main():
+    """ Print details of network interfaces """
+    for i in netifaces.interfaces():
+        print('\n**************Details of Interface - ' + i + ' *********************')
+        try:
+            print("MAC:", getMacAddress(i))
+            print("IP:", getIpAddress(i))
+        except:
+            print('Could not collect adapter information')
+
+main()
---
+++ smallscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Print a string to the environment output
+"""
+
+def main():
+    """Program Initiation"""
+
+    mynote = "print this string"
+    print(mynote)
+
+main()
